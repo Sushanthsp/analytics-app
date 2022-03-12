a7e62376afce6cf49dad3a1bedf72185d66825a3
Sushanthsp <sushanthspoojary777@gmail.com>
2022-03-12 09:44:42 +0530

    middleContainer completed

--- src/components/MiddleBarComponents/QuickStat.js
+++ src/components/MiddleBarComponents/QuickStat.js
@@ -1,10 +1,59 @@
-import React from 'react'
-import "./QuickStat.css"
+import React from "react";
+import "./QuickStat.css";
+import Button from "@mui/material/Button";
 
 function QuickStat() {
   return (
-    <div>QuickStat</div>
-  )
+    <div>
+      <div className="container">
+        <div className="Stat">
+        <div className="fStat">
+            <div className="icon">
+              <Button variant="contained">icon</Button>
+            </div>
+            <div className="numbers">
+              <h2 className="rank">
+                567
+              </h2>
+              <h4 className="bottom">
+               rank
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div className="Stat">
+        <div className="fStat">
+            <div className="icon">
+              <Button variant="contained">icon</Button>
+            </div>
+            <div className="numbers">
+              <h2 className="rank">
+                567
+              </h2>
+              <h4 className="bottom">
+               rank
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div className="Stat">
+        <div className="fStat">
+            <div className="icon">
+              <Button variant="contained">icon</Button>
+            </div>
+            <div className="numbers">
+              <h2 className="rank">
+                567
+              </h2>
+              <h4 className="bottom">
+               rank
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default QuickStat
+export default QuickStat;
--- src/components/Middlebar.js
+++ src/components/Middlebar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Middlebar.css";
 import TopBox from "./MiddleBarComponents/TopBox";
+import QuickStat from "./MiddleBarComponents/QuickStat";
 
 function Middlebar() {
   return (
     <div className="main">
       <div className="main_body">
         <div className="middlebar">
-         <TopBox/>
+          <TopBox />
+          <QuickStat/>
         </div>
 
         <div className="rightbar">
